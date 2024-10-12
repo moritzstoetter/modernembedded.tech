ab22caa7bfb131a57f058eaafbd61ecf2dc8254a
Tobias Arweiler <tobias@icebearlabs.com>
2024-10-12 17:44:24 +0200

    added working navbar mobile and desktop, adjusted theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   i18n: {
     defaultLocale: "de",
     locales: ["de", "en"],
